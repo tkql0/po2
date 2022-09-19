8a639f48f0b49dfa84e1d5138c268a2c79a969fc
tkql0 <k1122tkql0@gmail.com>
2022-09-20 07:16:39 +0900

    중간저잘19

--- 2.Scripts/Aiming_Camera.cs
+++ 2.Scripts/Aiming_Camera.cs
@@ -5,5 +5,5 @@ using Cinemachine;
 
 public class Aiming_Camera : MonoBehaviour
 {
-
+    // 카메라 타겟 스트립트의 순서를 따라가되 시야를 다르게
 }
--- 2.Scripts/Point.cs
+++ 2.Scripts/Point.cs
@@ -9,8 +9,55 @@ public class Point : MonoBehaviour
     public int curHealth;
     // 현재 체력
 
+    float range = 8f;
+    
+    public string PlayerTag = "Player";
+    public List<GameObject> PlayerList = new List<GameObject>();
+
     private void Start()
     {
         curHealth = PointHealth;
+    }
+
+    private void Update()
+    {
+        Spawn_Limit_Target();
+    }
+
+    void Spawn_Limit_Target()
+    {
+        PlayerList.Clear();
+        GameObject[] players = GameObject.FindGameObjectsWithTag(PlayerTag);
+        float shortesDistance = Mathf.Infinity;
+        foreach (GameObject Player in players)
+        {
+            float distanceTogameObject = Vector3.Distance(transform.position, Player.transform.position);
+            if (distanceTogameObject < shortesDistance)
+            {
+                shortesDistance = distanceTogameObject;
+                PlayerList.Add(Player);
+            }
+        }
+        if (PlayerList.Count >= 1)
+        {
+            for (int i = 0; i < PlayerList.Count; i++)
+            {
+                if (shortesDistance <= range)
+                {
+                    PlayerList[i].GetComponent<Castle_Spawn>().Spawn_Limit = true;
+                }
+                else
+                {
+                    PlayerList[i].GetComponent<Castle_Spawn>().Spawn_Limit = false;
+                    PlayerList.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    { // 그냥 시각적 효과 거리는 5만큼 지워도됨
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
