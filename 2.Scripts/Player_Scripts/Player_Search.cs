340236d6d855be380130161acb6ce51152551bdf
tkql0 <k1122tkql0@gmail.com>
2022-09-30 18:26:14 +0900

    중간저장41

--- 2.Scripts/Player_Scripts/Player_Search.cs
+++ 2.Scripts/Player_Scripts/Player_Search.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player_Search : MonoBehaviour
 {
-    public float range = 6f;
-    // 타겟을 탐색할 거리 // 레이를 쏠 거리
+    [SerializeField]
+    float Player_Seatch_ange;
+    // 타겟을 탐색할 거리
 
     public GameObject Danger;
 
@@ -20,21 +21,13 @@ public class Player_Search : MonoBehaviour
     void Search()
     {
         // 저장된 타겟을 초기화
-        Collider[] inTarget = Physics.OverlapSphere(transform.position, range, targetMask);
+        Collider[] inTarget = Physics.OverlapSphere(transform.position, Player_Seatch_ange, targetMask);
         // 범위안에 들어온 타겟레이어를 저장
 
         if(inTarget.Length >= 1)
-        {
             Danger.SetActive(true);
-        }
 
         else
             Danger.SetActive(false);
-    }
-
-    private void OnDrawGizmosSelected()
-    { // 그냥 시각적 효과 거리는 5만큼 지워도됨
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
